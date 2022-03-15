4ebfe7c846f21731a900d2ef09954959d15f94c1
Your Name <you@example.com>
2022-03-15 18:48:53 +0530

    Sad Mood

---
+++ test/java/com/bl/TestAnalyser.java
@@ -0,0 +1,35 @@
+package com.bl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestAnalyser
+{
+	/**
+	 * Identifies a method as a test method.
+	 */
+	@Test
+	/**
+	 * created method analyseSadMood() for getting mood as sad
+	 */
+	public void analyseSadMood() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		moodAnalyser.setMessage("I am in Sad Mood");
+		String actualResult = moodAnalyser.analyseMood();
+		Assert.assertEquals("SAD", actualResult);
+	}
+
+	@Test
+	/**
+	 * created method analyseAnyMood() for getting mood as Happy
+	 */
+	public void analyseAnyMood() 
+	{
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		moodAnalyser.setMessage("I am in any Mood");
+		String actualResult = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY", actualResult);
+	}
+}
+
+
